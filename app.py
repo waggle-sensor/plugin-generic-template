403c33692fffaa8e80adab9d108ed2061ace5bbc
seonghapark <seonghapark06@gmail.com>
2021-12-04 14:18:48 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,19 @@
 import whatever-lib
 
+def pre_a():
 
+def post_a():
+
+def preprocessing():
+  pre_a()
+  preprocessing lines
+
+def model_loading():
+  model loading lines
+
+def inference():
+  inference lines
+
+def postprocessting():
+  post_a()
+  postprocessing lines
