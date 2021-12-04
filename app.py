bdd520a9158225973396667c1760593943e9434d
seonghapark <seonghapark06@gmail.com>
2021-12-04 14:27:57 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,12 +4,15 @@ def pre_a():
 
 def post_a():
 
+def model_loading():
+  model loading lines
+
+def get_input():
+  get input lines
+
 def preprocessing():
   pre_a()
   preprocessing lines
-
-def model_loading():
-  model loading lines
 
 def inference():
   inference lines
@@ -19,7 +22,12 @@ def postprocessting():
   postprocessing lines
 
 if __name__ == "__main__":
-  preprocessing()
-  model_loading()
-  inference()
-  postprocessing()
+  my_plugin = Plugin(app_name=”my_edge_app”)
+
+  my_plugin.add_codeblock(model_loading)
+  my_plugin.add_codeblock(get_input)
+  my_plugin.add_codeblock(preprocessing)
+  my_plugin.add_codeblock(inference)
+  my_plugin.add_codeblock(postprocessing)
+
+  my_plugin.run()
